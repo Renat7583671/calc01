1ee1c429bb32ca8ac7b6bcd68d8c14de1da6b912
Renat <renat@ksyste.ms>
2015-11-20 00:51:27 +0200

    ne dodelan

--- app/src/main/java/com/example/renat/mycalculatortest003/MainActivity.java
+++ app/src/main/java/com/example/renat/mycalculatortest003/MainActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView textView;
+    EditText editText;
+    EditText editText2;
     Button button1;
     Button button2;
     Button button3;
@@ -23,12 +26,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button button13;
     Button button14;
     Button button15;
+    String mCount = "";
+    double a,b,c;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calc);
+        int a=0;
+        int b=0;
+        int c=0;
+        editText =(EditText)findViewById(R.id.editText);
+        editText2 =(EditText)findViewById(R.id.editText2);
+        textView =(TextView)findViewById(R.id.textView);
+
+
+
+
+
         textView = (TextView) findViewById(R.id.textView);
         button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
@@ -69,55 +85,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        a =Float.parseFloat(editText.getText().toString());
+        b =Float.parseFloat(editText2 .getText().toString());
         switch (v.getId()) {
-            case R.id.button1:
-                textView.setText("1");
-                break;
-            case R.id.button2:
-                textView.setText("2");
-                break;
-            case R.id.button3:
-                textView.setText("3");
-                break;
-            case R.id.button4:
-                textView.setText("4");
-                break;
-            case R.id.button5:
-                textView.setText("5");
-                break;
-            case R.id.button6:
-                textView.setText("6");
-                break;
-            case R.id.button7:
-                textView.setText("7");
-                break;
-            case R.id.button8:
-                textView.setText("8");
-                break;
-            case R.id.button9:
-                textView.setText("9");
-                break;
             case R.id.button10:
-                textView.setText("+");
-                break;
-            case R.id.button11:
-                textView.setText("0");
+                mCount = "+";
+                c = a + b;
                 break;
             case R.id.button12:
-                textView.setText("-");
+                mCount = "-";
+                c = a - b;
                 break;
             case R.id.button13:
-                textView.setText("*");
-                break;
-            case R.id.button14:
-                textView.setText("=");
+                mCount = "*";
+                c = a * b;
                 break;
             case R.id.button15:
-                textView.setText("/");
+                mCount = "/";
+                c = a / b;
                 break;
-
+            default:
+                break;
         }
-
+        textView.setText(a + " " + mCount + " " + b + " = " + c);
 
     }
 }
